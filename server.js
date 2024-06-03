381c6b20b58212bbba1e61b67b3fc9ed3222f2db
m1lequezinlean <mulequezinmaker@gmail.com>
2024-06-03 11:40:15 -0300

    server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const multer = require('multer');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, 'server')));
+
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
